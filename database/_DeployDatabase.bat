6df4c42a9d8bc8302a77dd37f0cf82cb82212f14
95nagyd <95.nagy.d@gmail.com>
2020-12-04 07:42:02 +0100

    db deploy script fix

--- database/_DeployDatabase.bat
+++ database/_DeployDatabase.bat
@@ -1,5 +1,6 @@
 @echo off
 echo Deploying MongoDB database...
+if not exist %cd%\\appDB\\data\ mkdir %cd%\\appDB\\data
 start mongod --dbpath %cd%\\appDB\\data
 call InitDatabase.bat
 call DatabaseCMD.bat
